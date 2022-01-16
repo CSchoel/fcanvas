b751eb3b65c2be332168f435d88959598c01fc4d
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-01-16 21:23:29 +0100

    implements first version of rectangle test

--- test/de/thm/mni/oop/fcanvas/FCanvasTest.java
+++ test/de/thm/mni/oop/fcanvas/FCanvasTest.java
@@ -1,13 +1,32 @@
+package de.thm.mni.oop.fcanvas;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.nio.file.Path;
+import java.io.IOException;
 
 public class FCanvasTest {
 
+    private static final Path IMAGE_DIR = Path.of(".");
+
     @Test
-    public void testRectangle() {
-        BufferedImage bi = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
-        System.out.println("Muh");
-        assertEquals(bi, bi);
+    public void testRectangle() throws IOException, InterruptedException {
+        FCanvas.show();
+        BufferedImage bi = new BufferedImage(FCanvas.getCanvasWidth(), FCanvas.getCanvasHeight(), BufferedImage.TYPE_INT_RGB);
+        bi.getGraphics().drawRect(10, 10, 100, 100);
+        FCanvas.drawRectangle(10, 10, 100, 100);
+        Thread.sleep(1000);
+        BufferedImage ref = FCanvas.gui.getPanel().toImage();
+        System.out.println(ref);
+        System.out.println(bi);
+        saveImage(bi, IMAGE_DIR.resolve("bi.png"));
+        saveImage(ref, IMAGE_DIR.resolve("ref.png"));
+        assertEquals(bi, ref);
+    }
+
+    public static void saveImage(BufferedImage bi, Path dest) throws IOException {
+        ImageIO.write(bi, "png", dest.toFile());
     }
 }
